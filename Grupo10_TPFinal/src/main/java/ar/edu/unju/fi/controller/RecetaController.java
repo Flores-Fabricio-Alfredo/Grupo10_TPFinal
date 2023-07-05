6c0ea84bf6d2412a662d28f1dcde33c2941e02c6
Farja Naima Blanca <Farja Naima Blanca@DESKTOP-RNP7A0T>
2023-07-05 16:06:48 -0300

    Controller y Entity
    Desde la rama "Creacion_Controller_Entity_Recetas" cree la capa
    Controller junto con Entity, para poder luego realizar el mapeo y poder
    cargar infromacion en el html de "Recetas.html"

--- Grupo10_TPFinal/src/main/java/ar/edu/unju/fi/controller/RecetaController.java
+++ Grupo10_TPFinal/src/main/java/ar/edu/unju/fi/controller/RecetaController.java
@@ -1,5 +1,18 @@
 package ar.edu.unju.fi.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
 public class RecetaController {
+	
+	@GetMapping("/recetas")
+	public String getRecetasPage() {
+		return "recetas";
+	}
 
 }
