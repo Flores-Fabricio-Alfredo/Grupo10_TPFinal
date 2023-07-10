2f062851cce0530d6c6f1bce147bebf339d855f7
Sofia Trinidad Yañez <Sofia Trinidad Yaez@DESKTOP-LC3B0A5>
2023-07-10 15:05:23 -0300

    edicion Logueo para que el admin pueda dirigirse a la tabla de gestion de datos

--- Grupo10_TPFinal/src/main/java/ar/edu/unju/fi/controller/LoginController.java
+++ Grupo10_TPFinal/src/main/java/ar/edu/unju/fi/controller/LoginController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 import ar.edu.unju.fi.entity.Login;
 
+import ar.edu.unju.fi.service.IUsuarioService;
 
 @Controller
 @RequestMapping("/")
@@ -16,6 +18,9 @@ public class LoginController {
 	
 	@Autowired
 	private Login login;
+	
+	@Autowired
+	private IUsuarioService usuarioService;
 	
 	@GetMapping({"/login"})
 	public String login(Model model) {
@@ -27,7 +32,10 @@ public class LoginController {
 	@PostMapping("/login")
 	public String validar(Model model, Login login) {
 		if(login.getUsuarioLogin().equals("admin") && login.getPasswordLogin().equals("123")) {
-			return "index";
+			//ModelAndView modelView = new ModelAndView("gestion_de_datos");
+			//modelView.setViewName("gestion_de_datos");
+			model.addAttribute("usuario", usuarioService.getLista());
+			return "gestion_de_datos";
 		}
 		model.addAttribute("error", "Acceso incorrecto");
 		return "login";
