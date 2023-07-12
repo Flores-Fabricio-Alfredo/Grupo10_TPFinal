ffd552d06e27765c20afacd99afbfff924fd93ad
NaimaFarja <130595315+NaimaFarja@users.noreply.github.com>
2023-07-12 14:21:47 -0300

    Documentacion de Recetas y Ingredientes

--- Grupo10_TPFinal/src/main/java/ar/edu/unju/fi/repository/IRecetaRepository.java
+++ Grupo10_TPFinal/src/main/java/ar/edu/unju/fi/repository/IRecetaRepository.java
@@ -7,11 +7,28 @@ import org.springframework.stereotype.Repository;
 
 import ar.edu.unju.fi.entity.Receta;
 
+/**
+ * @author Naima Farja
+ * IRecetaRepository es una interfaz que extiende CrudRepository para proporcionar métodos de acceso a datos para la entidad Receta.
+ * Proporciona métodos para buscar recetas por estado y por categoría.
+ */
 @Repository
 public interface IRecetaRepository extends CrudRepository<Receta, Long> {
 
-	public List<Receta> findByEstado(boolean estado);
-	
-	public List<Receta> findByCategoria(String categoria);
-	
+    /**
+     * Busca recetas por estado.
+     *
+     * @param estado el estado de las recetas a buscar
+     * @return una lista de recetas que cumplen con el estado especificado
+     */
+    public List<Receta> findByEstado(boolean estado);
+
+    /**
+     * Busca recetas por categoría.
+     *
+     * @param categoria la categoría de las recetas a buscar
+     * @return una lista de recetas que pertenecen a la categoría especificada
+     */
+    public List<Receta> findByCategoria(String categoria);
+
 }
--- Grupo10_TPFinal/src/main/java/ar/edu/unju/fi/service/IIngredienteService.java
+++ Grupo10_TPFinal/src/main/java/ar/edu/unju/fi/service/IIngredienteService.java
@@ -4,14 +4,39 @@ import java.util.List;
 
 import ar.edu.unju.fi.entity.Ingrediente;
 
+/**
+ * @author Naima Farja
+ * La interfaz IIngredienteService define los métodos para el servicio relacionado con los ingredientes.
+ * Proporciona métodos para obtener la lista de ingredientes, guardar un ingrediente, buscar un ingrediente por su ID,
+ * eliminar un ingrediente y obtener un ingrediente.
+ */
 public interface IIngredienteService {
 	
+    /**
+     * Obtiene la lista de ingredientes.
+     *
+     * @return una lista de ingredientes
+     */
 	List<Ingrediente> getListaingrediente();
-
+	 /**
+     * Guarda un ingrediente.
+     *
+     * @param ingred el ingrediente a guardar
+     */
+    
 	void guardar(Ingrediente ingred);
-
+	   /**
+     * Busca un ingrediente por su ID.
+     *
+     * @param id el ID del ingrediente a buscar
+     * @return el ingrediente encontrado, o null si no se encuentra ningún ingrediente con el ID especificado
+     */
 	Ingrediente getBy(Long id);
-
+    /**
+     * Elimina un ingrediente.
+     *
+     * @param ingredienteEncontrado el ingrediente a eliminar
+     */
 	void eliminar(Ingrediente ingredienteEncontrado);
 
 	Ingrediente getIngrediente();
