2e147ad0fa2903ab107b56f7695642e413c4b7ae
Sofia Trinidad Yañez <Sofia Trinidad Yaez@DESKTOP-LC3B0A5>
2023-07-12 01:32:42 -0300

    Correcions Correcciones de capa controller, entity, servicio y repository de pa pagina calculadora_imc.html

---
+++ Grupo10_TPFinal/src/main/java/ar/edu/unju/fi/controller/CalcularImcController.java
@@ -0,0 +1,85 @@
+package ar.edu.unju.fi.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ar.edu.unju.fi.entity.IndiceMasaCorporal;
+import ar.edu.unju.fi.entity.Registro;
+import ar.edu.unju.fi.entity.Usuario;
+import ar.edu.unju.fi.repository.IUsuarioRepository;
+import ar.edu.unju.fi.service.IIndiceMasaCorporalService;
+import ar.edu.unju.fi.service.IUsuarioService;
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/")
+public class CalcularImcController {
+
+	@Autowired
+	private IIndiceMasaCorporalService indiceMasaCorporalService;
+	@Autowired
+	private IUsuarioRepository usuarioRepository;
+	@Autowired
+	private IUsuarioService usuarioService;
+	@Autowired
+	private Usuario usuario;
+
+	@GetMapping("/calcular")
+	
+	public String getCalcularImcPage(Model model) {
+		model.addAttribute("mostrarBotonVolver", true);
+		model.addAttribute("imc", indiceMasaCorporalService.getIMC());
+		return "calculadora_imc";
+	}
+
+	
+	@PostMapping("/calculo")
+	public String calcularIMC(@RequestParam(value = "id") Long id, @RequestParam(value = "peso") Double peso,
+			Model model) {
+		Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
+		
+		if (optionalUsuario.isPresent()) {
+			Usuario usuarioEncontrado = optionalUsuario.get();
+			Double altura = usuarioEncontrado.getEstatura();
+			IndiceMasaCorporal imc = indiceMasaCorporalService.getIMC();
+			
+			imc.setImc(indiceMasaCorporalService.calcularIMC(peso, altura));
+			imc.setUsuarioImc(usuarioEncontrado.getNombre() + " " + usuarioEncontrado.getApellido());
+			imc.setUsuario(usuarioEncontrado);
+			
+			if (imc.getId() != null) {
+				imc.setId(imc.getId() + 1);
+			}
+		
+			indiceMasaCorporalService.guardarIMC(imc);
+			usuarioEncontrado.añadirImc(imc);
+			usuarioService.guardar(usuarioEncontrado);
+
+			model.addAttribute("resultadoIMC", imc.getImc());
+			model.addAttribute("usuarioEncontrado", usuarioEncontrado);
+		}
+		
+		if (optionalUsuario.isEmpty()) {
+			model.addAttribute("mensaje", "El codigo ingresado no pertenece a un usuario");
+			return "calculadora_imc";
+		}
+		return "ImcTabla";
+	}
+
+	@GetMapping("/eliminar/{id}")
+	public String eliminarUsuario(@PathVariable(value = "id") Long id) {
+		IndiceMasaCorporal imc = indiceMasaCorporalService.getBy(id);
+		indiceMasaCorporalService.eliminarIMC(imc);
+		return "redirect:/gestion";
+	}
+
+}
